8b28e8dac8bca9a8742b0dd8af01cbe9219dcfc4
Kiadawel <kimi.weldon@gmail.com>
2019-03-28 14:22:29 -0700

    makin' progress

---
+++ assignments/templeinnsuites/scripts/navigation.js
@@ -0,0 +1,54 @@
+function loadNav(){
+    var navLinks = ['Home','Temple','Reservation','Services','Contact'];
+    var navLoader = document.querySelector('#navloader');
+    var navList = document.createElement('ul');
+
+    for(i=0; i<navLinks.length; i++){
+        //simplify the page name for later use
+        var simplename = navLinks[i].toLowerCase();
+
+        //switch the weird one
+        if(simplename == 'home'){
+            simplename = 'index';
+        }
+
+        //check URL for which page we're currently on
+        var currentPage = window.location.href.search(simplename);
+
+        //if we're currently on the homepage, do not display the Home link
+        if(currentPage != -1 && simplename == 'index'){
+            continue;
+        }
+        //otherwise, continue to build the navigation
+        else{
+            var navItem = document.createElement('li'); 
+            var navAnchor = document.createElement('a');
+            var navIcon = document.createElement('img');
+            var navLabel = document.createElement('span');
+
+            var linkPath = simplename + '.html'; 
+            var iconPath = 'images/navicon_' + simplename + '.png';
+
+            navAnchor.setAttribute('href',linkPath);
+
+            navIcon.setAttribute('src',iconPath);
+            navIcon.setAttribute('alt',navLinks[i]);
+
+            navLabel.setAttribute('class','navtext');
+            navLabel.textContent = navLinks[i];
+
+            //add the icon and the link name to the link
+            navAnchor.appendChild(navIcon);
+            navAnchor.appendChild(navLabel);
+
+            //add the link to the list item
+            navItem.appendChild(navAnchor);
+
+            //add the list item to the list
+            navList.appendChild(navItem);
+        }
+        //once all list items are loaded, add the list to the container
+        navLoader.appendChild(navList);
+    }
+
+}
