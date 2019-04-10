6e1e37aeb998bf2c89c5eb315974cfc032e1b3a6
Kiadawel <kimi.weldon@gmail.com>
2019-04-10 00:39:04 -0700

    fixing links

--- assignments/templeinnsuites/scripts/pagebuilder.js
+++ assignments/templeinnsuites/scripts/pagebuilder.js
@@ -53,9 +53,9 @@ function loadFoot(){
         '<a href="attribution.html">Attribution</a>' +
         '</div>' +
         '<div class="social">' +
-            '<a href="http://www.facebook.com" target="_blank"><img src="images/facebook.png" alt="Facebook"></a>' +
-            '<a href="http://www.facebook.com" target="_blank"><img src="images/twitter.png" alt="Twitter"></a>' +
-            '<a href="http://www.facebook.com" target="_blank"><img src="images/instagram.png" alt="Instagram"></a>' +
+            '<a href="https://www.facebook.com" rel="noreferrer" target="_blank"><img src="images/facebook.png" alt="Facebook"></a>' +
+            '<a href="https://www.twitter.com" rel="noreferrer" target="_blank"><img src="images/twitter.png" alt="Twitter"></a>' +
+            '<a href="https://www.instagram.com" rel="noreferrer" target="_blank"><img src="images/instagram.png" alt="Instagram"></a>' +
         '</div>';
     document.getElementById('footloader').innerHTML = pageFooter;
 }
